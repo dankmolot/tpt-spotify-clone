7a535ffdd6046fa6e007bc80043e192d9b34cfff
Retro <44505837+dankmolot@users.noreply.github.com>
2025-07-28 21:59:20 +0300

    feat: remove testing

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,9 @@
-import { defineConfig } from 'vite'
-import viteReact from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import tsconfigPaths from 'vite-tsconfig-paths'
+import tailwindcss from "@tailwindcss/vite";
+import viteReact from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [tsconfigPaths(), viteReact(), tailwindcss()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-  },
-})
+    plugins: [tsconfigPaths(), viteReact(), tailwindcss()],
+});
