216356700d8b943d504419ecf5292fcc57535916
Retro <44505837+dankmolot@users.noreply.github.com>
2025-11-26 00:42:13 +0200

    feat: basic login layout + styles for TextField and Button

--- src/routes/auth/login.tsx
+++ src/routes/auth/login.tsx
@@ -1,17 +1,56 @@
 import { createFileRoute } from "@tanstack/react-router"
-import clsx from "clsx"
+import { Button, Form, Input, Label, TextField } from "react-aria-components"
+import { type SubmitHandler, useForm } from "react-hook-form"
 import { Logo } from "@/components/Logo"
+import { cn } from "@/lib/utils"
 import classes from "./login.module.scss"
 
 export const Route = createFileRoute("/auth/login")({
     component: RouteComponent,
 })
 
+interface Fields {
+    server: string
+    user: string
+    pass: string
+}
+
 function RouteComponent() {
+    const {
+        register,
+        handleSubmit,
+        watch,
+        formState: { errors },
+    } = useForm<Fields>()
+
+    const onSubmit: SubmitHandler<Fields> = (data) => {
+        console.log("eee")
+    }
+
     return (
-        <div className={clsx("panel", classes.loginBox)}>
+        <Form
+            className={cn("panel", classes.loginBox)}
+            onSubmit={handleSubmit(onSubmit)}
+        >
             <Logo />
             <h1>Welcome back</h1>
-        </div>
+
+            <TextField>
+                <Label>Server</Label>
+                <Input placeholder="https://demo.navidrome.org" type="url" />
+            </TextField>
+
+            <TextField>
+                <Label>User</Label>
+                <Input placeholder="demo" />
+            </TextField>
+
+            <TextField>
+                <Label>Pass</Label>
+                <Input placeholder="demo" type="password" />
+            </TextField>
+
+            <Button type="submit">Login</Button>
+        </Form>
     )
 }
