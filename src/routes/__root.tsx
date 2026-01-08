1d038a3aff315e7f8a9d725f0e752f411a0d1c7f
Retro <44505837+dankmolot@users.noreply.github.com>
2026-01-08 22:09:50 +0200

    feat: add router devtools back

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,17 +1,18 @@
-import { createRootRoute, Outlet } from "@tanstack/react-router";
+import { createRootRoute, Outlet } from "@tanstack/react-router"
+import { TanStackRouterDevtools } from "@tanstack/react-router-devtools"
 import TanStackQueryLayout from "@/integrations/tanstack-query/layout"
 
 export const Route = createRootRoute({
     component: RootLayout,
-});
+})
 
 function RootLayout() {
     return (
         <>
             <Outlet />
-            {/* <TanStackRouterDevtools /> */}
+            <TanStackRouterDevtools position="top-right" />
 
             <TanStackQueryLayout />
         </>
-    );
+    )
 }
