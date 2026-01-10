c1701e384102f2f424f7d8d602fd4e62b9a3343a
Retro <44505837+dankmolot@users.noreply.github.com>
2026-01-10 17:15:27 +0200

    feat: add ability to select a song on album page

--- src/routes/album.$albumID.tsx
+++ src/routes/album.$albumID.tsx
@@ -1,8 +1,10 @@
 import { useSuspenseQuery } from "@tanstack/react-query"
 import { createFileRoute } from "@tanstack/react-router"
+import { useShallow } from "zustand/react/shallow"
 import { Album } from "@/components/subsonic/Album"
 import { getQueryClient } from "@/integrations/tanstack-query/root-provider"
 import { getAlbumOptions } from "@/lib/queries/subsonic"
+import { usePlayerState } from "@/lib/state"
 
 export const Route = createFileRoute("/album/$albumID")({
     loader: ({ params: { albumID } }) =>
@@ -13,12 +15,17 @@ export const Route = createFileRoute("/album/$albumID")({
 function RouteComponent() {
     const { albumID } = Route.useParams()
     const { data: album } = useSuspenseQuery(getAlbumOptions({ id: albumID }))
+    const [songID, setSong] = usePlayerState(useShallow(s => [s.songID, s.setSong]))
 
     return (
         <>
             <Album album={album} />
             {album.song?.map((v) => (
-                <p key={v.id}>{v.title}</p>
+                <p key={v.id}>
+                    <button type="button" onClick={() => setSong(v.id)}>
+                        {v.title} {songID === v.id && "playing"}
+                    </button>
+                </p>
             ))}
         </>
     )
