fcdf3e4e45c2ad65034cae734ffbcb6e3da1acee
Retro <44505837+dankmolot@users.noreply.github.com>
2026-01-11 13:00:39 +0200

    feat: show current song with cover art and title + love icon WIP

--- src/components/player/CurrentSong.tsx
+++ src/components/player/CurrentSong.tsx
@@ -1,3 +1,32 @@
+import { useQuery } from "@tanstack/react-query"
+import { Heart } from "lucide-react"
+import { useEffect } from "react"
+import { getSongOptions } from "@/lib/queries/subsonic"
+import { CoverArt } from "../subsonic/CoverArt"
+import classes from "./CurrentSong.module.css"
+
 export function CurrentSong() {
-    return <div>Current song</div>
+    // const songID = usePlayerState((s) => s.songID)
+    const songID = "GWqhusYmlzzGuSsucaeYyr"
+    // const songID = "test"
+    const { data: song, error } = useQuery(getSongOptions({ id: songID }))
+
+    useEffect(() => {
+        if (!error) return
+
+        console.error("unable to load current song", error)
+    }, [error])
+
+    if (!song) return <div></div>
+
+    return (
+        <div className={classes.panel}>
+            <CoverArt id={songID} className={classes.coverArt} />
+            <div className={classes.info}>
+                <span className={classes.title}>{song?.title}</span>
+                <span className={classes.author}>{song?.artist}</span>
+            </div>
+            <Heart fill="white" height="1.25em" />
+        </div>
+    )
 }
