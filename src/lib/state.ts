3dcd4f93e4ea1056b2c12d3eb1b601e13053e7ab
Retro <44505837+dankmolot@users.noreply.github.com>
2026-01-13 00:47:57 +0200

    feat: integrate devtools into zustand player state

--- src/lib/state.ts
+++ src/lib/state.ts
@@ -1,11 +1,17 @@
 import { create } from "zustand"
+import { devtools } from "zustand/middleware"
 
 interface PlayerState {
     songID: string
     setSong: (id: string) => void
 }
 
-export const usePlayerState = create<PlayerState>()((set) => ({
-    songID: "",
-    setSong: (id) => set({ songID: id }),
-}))
+export const usePlayerState = create<PlayerState>()(
+    devtools(
+        (set) => ({
+            songID: "",
+            setSong: (id) => set({ songID: id }),
+        }),
+        { name: "Player" },
+    ),
+)
