b86d4acaca47df2fffe16a04db3ae594fe153735
dankmolot <dankmolot@p1ka.eu>
2026-02-19 11:59:48 +0200

    feat: add devenv support for development in nix environment

---
+++ devenv.nix
@@ -0,0 +1,21 @@
+{ ... }:
+
+{
+  languages.javascript = {
+    enable = true;
+    pnpm.enable = true;
+  };
+
+  # https://devenv.sh/basics/
+  # https://devenv.sh/packages/
+  # https://devenv.sh/languages/
+  # https://devenv.sh/processes/
+  # https://devenv.sh/services/
+  # https://devenv.sh/scripts/
+  # https://devenv.sh/basics/
+  # https://devenv.sh/tasks/
+  # https://devenv.sh/tests/
+  # https://devenv.sh/git-hooks/
+
+  # See full reference at https://devenv.sh/reference/options/
+}
