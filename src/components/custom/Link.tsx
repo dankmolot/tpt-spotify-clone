0526488d5f6f34eea988851b84ba3f2ba97eca51
dankmolot <dankmolot@p1ka.eu>
2026-02-23 13:01:20 +0200

    fix: Link with decorations had broken types fixed according https://tanstack.com/router/latest/docs/guide/custom-link

--- src/components/custom/Link.tsx
+++ src/components/custom/Link.tsx
@@ -1,4 +1,4 @@
-import { Link as RouterLink } from "@tanstack/react-router"
+import { type LinkComponent, Link as RouterLink } from "@tanstack/react-router"
 import type { ComponentPropsWithRef } from "react"
 import "./Link.css"
 import { cn } from "@/lib/utils"
@@ -6,7 +6,7 @@ import { cn } from "@/lib/utils"
 export const RawLink = RouterLink
 export type RawLinkProps = ComponentPropsWithRef<typeof RawLink>
 
-export function Link({ className, ...props }: RawLinkProps) {
-    return <RawLink className={cn("link", className)} {...props} />
+export const Link: LinkComponent<typeof HTMLAnchorElement> = (props) => {
+    return <RawLink {...props} className={cn("link", props.className)} />
 }
 export type LinkProps = ComponentPropsWithRef<typeof Link>
