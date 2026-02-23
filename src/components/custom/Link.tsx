5092018dc9991125ec5b25b2a8e407443dc2931f
dankmolot <dankmolot@p1ka.eu>
2026-02-23 12:38:46 +0200

    feat: create RawLink without decorations

--- src/components/custom/Link.tsx
+++ src/components/custom/Link.tsx
@@ -1,6 +1,12 @@
 import { Link as RouterLink } from "@tanstack/react-router"
 import type { ComponentPropsWithRef } from "react"
 import "./Link.css"
+import { cn } from "@/lib/utils"
 
-export const Link = RouterLink
+export const RawLink = RouterLink
+export type RawLinkProps = ComponentPropsWithRef<typeof RawLink>
+
+export function Link({ className, ...props }: RawLinkProps) {
+    return <RawLink className={cn("link", className)} {...props} />
+}
 export type LinkProps = ComponentPropsWithRef<typeof Link>
--- src/components/subsonic/Item.tsx
+++ src/components/subsonic/Item.tsx
@@ -2,7 +2,7 @@ import type { ClassValue } from "clsx"
 import type { ComponentPropsWithRef } from "react"
 import type { Playlist } from "@/lib/api/subsonic/schemas"
 import { cn } from "@/lib/utils"
-import { Link } from "../custom/Link"
+import { RawLink } from "../custom/Link"
 import { CoverArt } from "./CoverArt"
 import classes from "./Item.module.css"
 
@@ -51,13 +51,13 @@ export interface PlaylistItemProps extends ComponentPropsWithRef<"div"> {
 
 export function PlaylistItem({ playlist, theme, ...props }: PlaylistItemProps) {
     return (
-        <Link to="/playlist/$playlistID" params={{ playlistID: playlist.id }} className={cn(classes.link, theme?.link)}>
+        <RawLink to="/playlist/$playlistID" params={{ playlistID: playlist.id }} className={cn(classes.link, theme?.link)}>
             <Item theme={theme?.item} coverID={playlist.coverArt ?? ""} {...props}>
                 <span className={cn(classes.title, theme?.title)}>
                     {playlist.name}
                 </span>
                 {playlist.owner && <span className={cn(classes.description, theme?.owner)}>Playlist • {playlist.owner}</span>}
             </Item>
-        </Link>
+        </RawLink>
     )
 }
