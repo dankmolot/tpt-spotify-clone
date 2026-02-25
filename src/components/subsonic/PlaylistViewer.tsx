e901835ca2e9d551c9a724e92b46cd265d9f2fd8
dankmolot <dankmolot@p1ka.eu>
2026-02-25 03:59:24 +0200

    fix: left sidebar is not shown during the load

--- src/components/subsonic/PlaylistViewer.tsx
+++ src/components/subsonic/PlaylistViewer.tsx
@@ -5,13 +5,12 @@ import classes from "./PlaylistViewer.module.css"
 
 export default function PlaylistViewer() {
     const { data: playlists } = useQuery(getPlaylistsOptions())
-    if (!playlists) return
 
     return (
         <div className={classes.viewer}>
             <span className={classes.title}>Your Library</span>
             <div className={classes.list}>
-                {playlists.map((p) => (
+                {playlists?.map((p) => (
                     <PlaylistItem key={p.id} playlist={p} />
                 ))}
             </div>
