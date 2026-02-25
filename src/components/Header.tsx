ed2fc02c17bbfa69fdd6abcf2136021a0b138638
dankmolot <dankmolot@p1ka.eu>
2026-02-25 09:44:21 +0200

    feat: make header placeholders

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,4 @@
+import { HomeIcon, SearchIcon, UserIcon } from "lucide-react"
 import { Link } from "./custom/Link"
 import classes from "./Header.module.css"
 import { Logo } from "./Logo"
@@ -9,8 +10,21 @@ export function Header() {
                 <Logo />
             </Link>
 
-            <div>search</div>
-            <div>user</div>
+            <div className={classes.middle}>
+                <Link to="/" raw className={classes.home}>
+                    <HomeIcon />
+                </Link>
+                <div className={classes.search}>
+                    <SearchIcon className={classes.icon} />
+                    <span className={classes.placeholder}>
+                        What do you want to play?
+                    </span>
+                </div>
+            </div>
+
+            <div className={classes.user}>
+                <UserIcon />
+            </div>
         </div>
     )
 }
